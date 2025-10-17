8c0e270bfceb3b3a9ff10d2419e9e89442925d57
yankewei <yankewei1993@gmail.com>
2025-10-18 00:50:56 +0800

    Fix ts lint

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -38,7 +38,10 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn("text-lg font-semibold leading-none tracking-tight", className)}
+    className={cn(
+      "text-lg font-semibold leading-none tracking-tight",
+      className,
+    )}
     {...props}
   />
 ));
@@ -76,4 +79,11 @@ const CardFooter = React.forwardRef<
 ));
 CardFooter.displayName = "CardFooter";
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+};
